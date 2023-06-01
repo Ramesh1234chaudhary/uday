53c65f66ff91048bab29c23a9d6eee9315dda245
ramesh chaudhary <romanchaudhary01994@gmail.com>
2023-06-01 16:29:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import React, { useState } from 'react'
 
-import './App.css';
-
-function App() {
+const App = () => {
+  const option=["cricket","footbal","hockey"]
+  const days=["weekday","weekend"]
+  const[choose,setChoose]=useState()
+  const[day,setDays]=useState()
   return (
-    <div className="App">
-     <h1>hellow world</h1>
-    </div>
-  );
-}
+    <div>
+      <h1>which game</h1>
+      {
+        option.map((val)=>{
+          return <div><input type="radio" name="options"  value={val} onChange={(e)=>setChoose(e.target.value)}/>
+          <label for="val">{val}</label></div>
+        })
+      }
+      
+      <h1>which day</h1>
+      {
+        days.map((val)=>{
+          return <div><input type="radio"  name="options"  value={val} onChange={(e)=>setDays(e.target.value)}/>
+          <label for="val">{val}</label></div>
+        })
+      }
+      
 
-export default App;
+      
+      <h1>you will play</h1>
+      <p>{choose} on {day}</p>  
+        
+    </div>
+  )
+}   
+
+export default App
